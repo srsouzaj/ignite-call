6525b9eff8a5b3aa3d20fdfc5a4b9151295edc65
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-28 19:07:28 -0300

    feat: add ClaimUsernameForm

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,29 +3,31 @@ import Image from 'next/image'
 import { Container, Hero, Preview } from './styles'
 
 import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
-    return (
-        <Container>
-            <Hero>
-                <Heading as="h1" size="4xl">
-                    Agendamento descomplicado
-                </Heading>
-                <Text size="xl">
-                    Conecte seu calendário e permita que as pessoas marquem agendamentos
-                    no seu tempo livre.
-                </Text>
-            </Hero>
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUsernameForm />
+      </Hero>
 
-            <Preview>
-                <Image
-                    src={previewImage}
-                    height={400}
-                    quality={100}
-                    priority
-                    alt="Calendário simbolizando aplicação em funcionamento"
-                />
-            </Preview>
-        </Container>
-    )
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
 }
