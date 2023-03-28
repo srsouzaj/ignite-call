9e685681d1a7cd1a5fed7c77f0e255631aea8fa2
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-28 16:46:47 -0300

    fix: seo and deploy date

--- src/models/interfaces/Calendar.interface.ts
+++ src/models/interfaces/Calendar.interface.ts
@@ -1,19 +1,21 @@
+import dayjs from 'dayjs'
+
 export interface CalendarWeek {
-    week: number
-    days: Array<{
-        date: dayjs.Dayjs
-        disabled: boolean
-    }>
+  week: number
+  days: Array<{
+    date: dayjs.Dayjs
+    disabled: boolean
+  }>
 }
 
 export type CalendarWeeks = CalendarWeek[]
 
 export interface BlockedDates {
-    blockedWeekDays: number[]
-    blockedDates: number[]
+  blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 export interface CalendarInterface {
-    selectedDate: Date | null
-    onDateSelected: (date: Date) => void
+  selectedDate: Date | null
+  onDateSelected: (date: Date) => void
 }
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/react-query'
+import { DefaultSeo } from 'next-seo'
 import '../lib/dayjs'
 
 globalStyles()
@@ -14,6 +15,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.ignite-call.rocketseat.com.br',
+            siteName: 'Ignite Call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
